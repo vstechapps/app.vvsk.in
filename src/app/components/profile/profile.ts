9edd9ffee09b95d8aa27f9bc076055d86c0f49ec
VS Tech Apps <vstechapps@gmail.com>
2025-11-30 08:28:31 +0530

    updated ui

--- src/app/components/profile/profile.ts
+++ src/app/components/profile/profile.ts
@@ -2,11 +2,17 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, MatListModule, MatDividerModule],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
