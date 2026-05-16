bbb8f4d8f5c293ff665578ba53392ffc2a52407e
VS Tech Apps <vstechapps@gmail.com>
2026-05-17 01:07:24 +0530

    up

--- src/app/components/install-app/install-app.ts
+++ src/app/components/install-app/install-app.ts
@@ -18,14 +18,16 @@ export class InstallApp implements OnInit {
   }
 
   ngOnInit() {
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent the mini-infobar from appearing on mobile
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      this.deferredPrompt = e;
-      // Update UI notify the user they can install the PWA
-      this.showInstallButton.set(true);
-    });
+    if (!this.isStandalone()) {
+      window.addEventListener('beforeinstallprompt', (e) => {
+        // Prevent the mini-infobar from appearing on mobile
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        this.deferredPrompt = e;
+        // Update UI notify the user they can install the PWA
+        this.showInstallButton.set(true);
+      });
+    }
   }
 
   async installPwa() {
@@ -37,11 +39,9 @@ export class InstallApp implements OnInit {
       const { outcome } = await this.deferredPrompt.userChoice;
       if (outcome === 'accepted') {
         console.log('User accepted the install prompt');
-        localStorage.setItem("APP_INSTALLED", "true");
         this.showInstallButton.set(false);
       } else {
         console.log('User dismissed the install prompt');
-        localStorage.setItem("APP_INSTALLED", "false");
       }
       // We've used the prompt, and can't use it again, throw it away
       this.deferredPrompt = null;
